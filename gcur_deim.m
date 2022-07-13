1db4a77eab3cdde622623ee1a41393b558a0deea
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2022-07-13 11:22:59 +0200

    Update gcur_deim.m

--- gcur_deim.m
+++ gcur_deim.m
@@ -11,18 +11,22 @@ function [C_A, C_B, R_A, R_B, M, N] = cur_pair_deim(A, B, k, U, V, X)
 
 if nargin < 3 || isempty(k), k = 2; end
 [U, V, X, ~, ~] = gsvd(A,B,0);
-U = fliplr(U(:,end-k+1:end));       % Select largest (C,S) pairs
-V = fliplr(V(:,end-k+1:end));
-X = fliplr(X(:,end-k+1:end));
+U = fliplr(U);       % Select largest (C,S) pairs
+V = fliplr(V);
+X = fliplr(X);
 
-
+icol=zeros(1:k);
+iA=zeros(1:k);
+iB=zeros(1:k);
 for j = 1:k
   [~, icol(j)] = max(abs(X(:,j)));    % Iterative selection and projection
   [~, iA(j)] = max(abs(U(:,j)));
   [~, iB(j)] = max(abs(V(:,j)));
-  X(:,j+1:k) = X(:,j+1:k) - X(:,1:j) * (X(icol,1:j) \ X(icol,j+1:k));
-  U(:,j+1:k) = U(:,j+1:k) - U(:,1:j) * (U(iA,1:j) \ U(iA,j+1:k));
-  V(:,j+1:k) = V(:,j+1:k) - V(:,1:j) * (V(iB,1:j) \ V(iB,j+1:k));
+  if j<k
+    X(:,j+1) = X(:,j+1) - X(:,1:j) * (X(icol(1:j),1:j) \ X(icol,j+1));
+    U(:,j+1) = U(:,j+1) - U(:,1:j) * (U(iA(1:j),1:j) \ U(iA,j+1));
+    V(:,j+1) = V(:,j+1) - V(:,1:j) * (V(iB(1:j),1:j) \ V(iB,j+1));
+  end
 end
 C_A = A(:,icol);  
 C_B = B(:,icol);  
