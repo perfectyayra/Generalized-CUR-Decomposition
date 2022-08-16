4f782b1de2d64eb01c7a13befd5e40dc23e5d0db
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2022-08-16 04:20:17 +0200

    Update gcur_deim.m

--- gcur_deim.m
+++ gcur_deim.m
@@ -1,31 +1,35 @@
-function [C_A, C_B, R_A, R_B, M, N] = cur_pair_deim(A, B, k, U, V, X)
+function [icol, iA, iB, M, N] = gcur_deim(A, B, k)
 
 %CUR_PAIR_DEIM  DEIM incurred CUR decomposition for matrix pairs
 % [function [irow, iA, iB, M, N] = gcur_deim(A, B, k)
-%
+% Matrix A and B should have same number of columns
+% k is the desired rank of the approximation 
+% icol is the column indices selected
+% iA is the row indices of matrix A that has been selected
+% iB is the row indices of matrix B that has been selected
 % CA = A(:,icol);  CB = B(:,icol);  RA = A(iA,:);  RB = B(iB,:);
-%
+% M and N are the middle matrix of the gcur decomposition of A and B, respectively
 % See also cur_deim
 %
-% Reference: Gidisu and Hochstenbach 2021
+% Reference: Gidisu and Hochstenbach 2022 (https://doi.org/10.1137/21M1432119)
 
-if nargin < 3 || isempty(k), k = 2; end
-[U, V, X, ~, ~] = gsvd(A,B,0);
+
+[U, V, Y, ~, ~] = gsvd(A,B,0);  %matlab gsvd implementation
 U = fliplr(U);       % Select largest (C,S) pairs
 V = fliplr(V);
-X = fliplr(X);
+Y = fliplr(Y);
 
 icol=zeros(1:k);
 iA=zeros(1:k);
 iB=zeros(1:k);
 for j = 1:k
-  [~, icol(j)] = max(abs(X(:,j)));    % Iterative selection and projection
+  [~, icol(j)] = max(abs(Y(:,j)));    % Iterative selection and projection
   [~, iA(j)] = max(abs(U(:,j)));
   [~, iB(j)] = max(abs(V(:,j)));
   if j<k
-    X(:,j+1) = X(:,j+1) - X(:,1:j) * (X(icol(1:j),1:j) \ X(icol,j+1));
-    U(:,j+1) = U(:,j+1) - U(:,1:j) * (U(iA(1:j),1:j) \ U(iA,j+1));
-    V(:,j+1) = V(:,j+1) - V(:,1:j) * (V(iB(1:j),1:j) \ V(iB,j+1));
+    Y(:,j+1) = Y(:,j+1) - Y(:,1:j) * (Y(icol(1:j),1:j) \ Y(icol(1:j),j+1));
+    U(:,j+1) = U(:,j+1) - U(:,1:j) * (U(iA(1:j),1:j) \ U(iA(1:j),j+1));
+    V(:,j+1) = V(:,j+1) - V(:,1:j) * (V(iB(1:j),1:j) \ V(iB(1:j),j+1));
   end
 end
 C_A = A(:,icol);  
